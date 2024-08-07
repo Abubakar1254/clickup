9249a354bd66b28877bc8e1c30fe69adc07c534a
Abubakar1254 <Abubakar.innovativeally@gmail.com>
2024-08-07 20:42:29 +0500

    8/7/2024 8:42

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+function halfbar() {
+    // // document.getElementById("half_sidebar").style.width = "500px";
+    // document.getElementById("half_sidebar").style.marginLeft = "-130px";
+    
+    // document.getElementById("half_sidebar").style.position = "-130px";
+}
+
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+}
